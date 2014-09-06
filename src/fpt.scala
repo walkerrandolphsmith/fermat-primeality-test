3e13e24e095a3a8235fd444381ff3b9da60b4819
Walker Randolph Smith <walker@wearesoftwarecraftsmen.com>
2014-09-06 11:01:40 -0400

    Removed printHeader method.

--- src/fpt.scala
+++ src/fpt.scala
@@ -6,50 +6,25 @@ import scala.util.Random
 
 object fptRunner {
 
-  def printHeader(): String = {
-    var sb = new StringBuilder();
-
-    sb.append("---------------     --------------      _______________\n");
-    sb.append("---------------     ---------------     _______________\n");
-    sb.append("---------------     ----       ----     _______________\n");
-    sb.append("----                ----       ----          _____     \n");
-    sb.append("----                ----       ----          _____     \n");
-    sb.append("---------------     ---------------          _____     \n");
-    sb.append("---------------     -------------            _____     \n");
-    sb.append("---------------     -----------              _____     \n");
-    sb.append("----                -----                    _____     \n");
-    sb.append("----                -----                    _____     \n");
-    sb.append("----                -----                    _____     \n");
-    sb.append("----                -----                    _____     \n");
-    sb.append("----                -----                    _____     \n");
-
-    sb.append("             Fermat's Primality Test                   \n");
-    return sb.toString();
-  }
-
-  def fermatTest(n:BigInteger, maxIterations:BigInteger): Boolean = {
+  def fermatTest(n: BigInteger, maxIterations: BigInteger): Boolean = {
 
     //refactor for k iterations
     var a = maxIterations;
 
-    while(a.compareTo(BigInteger.ZERO) > 0 && a != BigInteger.ONE) {
+    while (a.compareTo(BigInteger.ZERO) > 0 && a != BigInteger.ONE) {
       //refactor to randomly select a
       a = a.subtract(BigInteger.ONE);
-        val r = a.modPow(maxIterations, n);
-        println("Iteration: " + a + " r = " + r);
-        if (!r.equals(BigInteger.ONE)) return false;
+      val r = a.modPow(maxIterations, n);
+      println("Iteration: " + a + " r = " + r);
+      if (!r.equals(BigInteger.ONE)) return false;
     }
     return true;
-
   }
 
   def main(args: Array[String]) {
-    println(printHeader());
     val sut = new BigInteger("31");
     val isPrime = fermatTest(sut, sut.subtract(BigInteger.ONE));
 
-    if(isPrime)  println("Likely Prime") else println("Composite");
-
-
+    if (isPrime) println("Likely Prime") else println("Composite");
   }
 }
